30c04f6bbf07364b69b98a1e893e0db668fecd94
Khoa109dn <Khoanguyen2006tpk@gmail.com>
2026-02-25 21:19:42 +0700

    Project

--- project_master/Src/main.c
+++ project_master/Src/main.c
@@ -1,6 +1,6 @@
 #include "stm32f103xb.h"
 
-// Cấu hình Clock 72MHz
+
 void SystemClock_Config(void) {
     RCC->CR |= RCC_CR_HSEON;
     while (!(RCC->CR & RCC_CR_HSERDY));
@@ -15,47 +15,46 @@ void Init_NodeA(void) {
     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_AFIOEN;
     RCC->APB1ENR |= RCC_APB1ENR_CAN1EN | RCC_APB1ENR_I2C1EN;
 
-    // CAN Pins: PA11 (RX), PA12 (TX)
     GPIOA->CRH &= ~(0xFF << 12);
     GPIOA->CRH |= (0x04 << 12) | (0x0B << 16);
 
-    // I2C1 Pins: PB6 (SCL), PB7 (SDA)
-    GPIOB->CRL |= (0xFF << 24); // AF Open-drain
 
-    // Cấu hình CAN 500kbps
+    GPIOB->CRL |= (0xFF << 24); 
+
+   
     CAN1->MCR |= CAN_MCR_INRQ;
     while (!(CAN1->MSR & CAN_MSR_INAK));
     CAN1->BTR = (12 << 16) | (1 << 20) | (8 - 1);
     CAN1->MCR &= ~CAN_MCR_INRQ;
     while (CAN1->MSR & CAN_MSR_INAK);
 
-    // Cấu hình I2C1 (Cơ bản cho AHT20)
+ 
     I2C1->CR2 = 36;
     I2C1->CCR = 180;
     I2C1->CR1 |= I2C_CR1_PE;
 }
 
 void CAN_SendTemp(uint8_t temp) {
-    CAN1->sTxMailBox[0].TIR = (0x123 << 21); // ID 0x123
-    CAN1->sTxMailBox[0].TDTR = 1;           // Độ dài 1 byte
-    CAN1->sTxMailBox[0].TDLR = temp;        // Dữ liệu nhiệt độ
-    CAN1->sTxMailBox[0].TIR |= CAN_TI0R_TXRQ; // Gửi
+    CAN1->sTxMailBox[0].TIR = (0x123 << 21); 
+    CAN1->sTxMailBox[0].TDTR = 1;          
+    CAN1->sTxMailBox[0].TDLR = temp;        
+    CAN1->sTxMailBox[0].TIR |= CAN_TI0R_TXRQ;
 }
 
 int main(void) {
     SystemClock_Config();
     Init_NodeA();
 
-    // Giả lập đọc AHT20 (Bạn thay bằng hàm đọc I2C thực tế)
+    
     uint8_t mock_temp = 30;
 
     while (1) {
         CAN_SendTemp(mock_temp);
 
-        // Giả lập nhiệt độ tăng dần để test
+        
         mock_temp++;
         if(mock_temp > 45) mock_temp = 25;
 
-        for(int i=0; i<1000000; i++); // Delay tạm
+        for(int i=0; i<1000000; i++); 
     }
 }
--- project_slave/Src/main.c
+++ project_slave/Src/main.c
@@ -15,30 +15,30 @@ void Init_NodeB(void) {
     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_AFIOEN | RCC_APB2ENR_USART1EN;
     RCC->APB1ENR |= RCC_APB1ENR_CAN1EN | RCC_APB1ENR_TIM2EN | RCC_APB1ENR_I2C1EN;
 
-    // GPIO Config (PA0: PWM, PA9: TX, PA11/12: CAN)
-    GPIOA->CRL |= (0x0B << 0);   // PA0 - TIM2 CH1
-    GPIOA->CRH |= (0x0B << 4);   // PA9 - UART1 TX
-    GPIOA->CRH |= (0x04 << 12) | (0x0B << 16); // PA11/12 - CAN
+   
+    GPIOA->CRL |= (0x0B << 0);  
+    GPIOA->CRH |= (0x0B << 4);   
+    GPIOA->CRH |= (0x04 << 12) | (0x0B << 16); 
 
-    // UART1 115200
+   
     USART1->BRR = 0x271;
     USART1->CR1 |= USART_CR1_TE | USART_CR1_UE;
 
-    // TIM2 Servo PWM (20ms)
+   
     TIM2->PSC = 72 - 1;
     TIM2->ARR = 20000 - 1;
     TIM2->CCMR1 |= (6 << 4);
     TIM2->CCER |= TIM_CCER_CC1E;
     TIM2->CR1 |= TIM_CR1_CEN;
 
-    // CAN 500kbps & Filter
+   
     CAN1->MCR |= CAN_MCR_INRQ;
     while (!(CAN1->MSR & CAN_MSR_INAK));
     CAN1->BTR = (12 << 16) | (1 << 20) | (8 - 1);
     CAN1->MCR &= ~CAN_MCR_INRQ;
 
     CAN1->FMR |= CAN_FMR_FINIT;
-    CAN1->sFilterRegister[0].FR1 = 0; // Nhận hết
+    CAN1->sFilterRegister[0].FR1 = 0;
     CAN1->FA1R |= CAN_FA1R_FACT0;
     CAN1->FMR &= ~CAN_FMR_FINIT;
 }
@@ -62,15 +62,15 @@ int main(void) {
     while (1) {
         if (CAN1->RF0R & CAN_RF0R_FMP0) {
             uint8_t temp = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR & 0xFF);
-            CAN1->RF0R |= CAN_RF0R_RFOM0; // Release FIFO
+            CAN1->RF0R |= CAN_RF0R_RFOM0; 
 
-            int angle = (temp > 35) ? 0 : 90; // Logic dừng ở 0 độ nếu > 35C
+            int angle = (temp > 35) ? 0 : 90; 
             Set_Servo(angle);
 
             sprintf(buf, "Temp: %d C | Servo: %d deg\r\n", temp, angle);
             UART_Send(buf);
 
-            // OLED: Cần gửi lệnh I2C tại đây để hiện 'angle'
+           
         }
     }
 }
