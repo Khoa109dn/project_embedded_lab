d29d1760f5002d93c218d314102d9e05f27c17d8
Khoa109dn <Khoanguyen2006tpk@gmail.com>
2026-02-25 21:13:12 +0700

    pj

---
+++ project_slave/Debug/Src/main.d
@@ -0,0 +1,13 @@
+Src/main.o: ../Src/main.c \
+ F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Include/core_cm3.h \
+ F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Include/cmsis_version.h \
+ F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Include/core_cm3.h:
+F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Include/cmsis_version.h:
+F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+F:/STM32Cube_FW_F1_V1.8.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
---
+++ project_slave/Src/main.c
@@ -0,0 +1,76 @@
+#include "stm32f103xb.h"
+#include <stdio.h>
+
+void SystemClock_Config(void) {
+    RCC->CR |= RCC_CR_HSEON;
+    while (!(RCC->CR & RCC_CR_HSERDY));
+    RCC->CFGR |= RCC_CFGR_PLLSRC | RCC_CFGR_PLLMULL9;
+    RCC->CR |= RCC_CR_PLLON;
+    while (!(RCC->CR & RCC_CR_PLLRDY));
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+}
+
+void Init_NodeB(void) {
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_AFIOEN | RCC_APB2ENR_USART1EN;
+    RCC->APB1ENR |= RCC_APB1ENR_CAN1EN | RCC_APB1ENR_TIM2EN | RCC_APB1ENR_I2C1EN;
+
+    // GPIO Config (PA0: PWM, PA9: TX, PA11/12: CAN)
+    GPIOA->CRL |= (0x0B << 0);   // PA0 - TIM2 CH1
+    GPIOA->CRH |= (0x0B << 4);   // PA9 - UART1 TX
+    GPIOA->CRH |= (0x04 << 12) | (0x0B << 16); // PA11/12 - CAN
+
+    // UART1 115200
+    USART1->BRR = 0x271;
+    USART1->CR1 |= USART_CR1_TE | USART_CR1_UE;
+
+    // TIM2 Servo PWM (20ms)
+    TIM2->PSC = 72 - 1;
+    TIM2->ARR = 20000 - 1;
+    TIM2->CCMR1 |= (6 << 4);
+    TIM2->CCER |= TIM_CCER_CC1E;
+    TIM2->CR1 |= TIM_CR1_CEN;
+
+    // CAN 500kbps & Filter
+    CAN1->MCR |= CAN_MCR_INRQ;
+    while (!(CAN1->MSR & CAN_MSR_INAK));
+    CAN1->BTR = (12 << 16) | (1 << 20) | (8 - 1);
+    CAN1->MCR &= ~CAN_MCR_INRQ;
+
+    CAN1->FMR |= CAN_FMR_FINIT;
+    CAN1->sFilterRegister[0].FR1 = 0; // Nhận hết
+    CAN1->FA1R |= CAN_FA1R_FACT0;
+    CAN1->FMR &= ~CAN_FMR_FINIT;
+}
+
+void Set_Servo(int angle) {
+    TIM2->CCR1 = 500 + (angle * 2000 / 180);
+}
+
+void UART_Send(char *str) {
+    while(*str) {
+        while(!(USART1->SR & USART_SR_TXE));
+        USART1->DR = *str++;
+    }
+}
+
+int main(void) {
+    SystemClock_Config();
+    Init_NodeB();
+    char buf[50];
+
+    while (1) {
+        if (CAN1->RF0R & CAN_RF0R_FMP0) {
+            uint8_t temp = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR & 0xFF);
+            CAN1->RF0R |= CAN_RF0R_RFOM0; // Release FIFO
+
+            int angle = (temp > 35) ? 0 : 90; // Logic dừng ở 0 độ nếu > 35C
+            Set_Servo(angle);
+
+            sprintf(buf, "Temp: %d C | Servo: %d deg\r\n", temp, angle);
+            UART_Send(buf);
+
+            // OLED: Cần gửi lệnh I2C tại đây để hiện 'angle'
+        }
+    }
+}
